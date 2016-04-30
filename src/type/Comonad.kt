63934a4890460a5afa6a9e5713bfc5cf61953846
Yuji Ueki <unhappychoice@gmail.com>
2016-05-01 03:48:27 +0900

    add comonad

---
+++ src/type/Comonad.kt
@@ -0,0 +1,8 @@
+package type
+
+import K1
+
+interface Comonad<µ>: Copointed<µ>, Functor<µ> {
+  fun <A> duplicate(v: K1<µ, A>): K1<µ, K1<µ, A>>
+  fun <A, B> extend(f: (K1<µ, A>) -> B, v: K1<µ, A>): K1<µ, B> = fmap(f, duplicate(v))
+}
