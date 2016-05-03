0e6cd212ce4974449ef42724dfcce9d2143c10a3
Yuji Ueki <unhappychoice@gmail.com>
2016-05-03 11:06:15 +0900

    refactor state

--- src/data/state/StateMonad.kt
+++ src/data/state/StateMonad.kt
@@ -3,26 +3,7 @@ package data.state
 import K1
 import type.monad.Monad
 
-interface StateMonad<S>: Monad<K1<State.µ, S>> {
-  override fun <A> pure(v: A): State<S, A> = State { s -> Pair(v, s) }
-
-  override fun <A, B> fmap(f: (A) -> B, v: K1<K1<State.µ, S>, A>): State<S, B> {
-    return State { s ->
-      val (value, s2) = State.narrow(v).value(s)
-      Pair(f(value), s2)
-    }
-  }
-
-  override fun <A, B> ap(f: K1<K1<State.µ, S>, (A) -> B>, v: K1<K1<State.µ, S>, A>): State<S, B> {
-    val state = State.narrow(v)
-    val statef = State.narrow(f)
-
-    return State { s ->
-      val (value, s2) = statef.value(s)
-      fmap(value, state).value(s2)
-    }
-  }
-
+interface StateMonad<S>: Monad<K1<State.µ, S>>, StateApplicative<S>, StateFunctor<S> {
   override fun <A> join(v: K1<K1<State.µ, S>, K1<K1<State.µ, S>, A>>): State<S, A> {
     val state = State.narrow(v)
     return State { s ->
