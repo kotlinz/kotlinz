cc0e36a751fb79411b2b5b74d32eaadef4523db8
Yuji Ueki <unhappychoice@gmail.com>
2016-05-06 09:34:52 +0900

    fix monad zip

--- src/type/monad/MonadZip.kt
+++ src/type/monad/MonadZip.kt
@@ -3,7 +3,7 @@ package type.monad
 import K1
 
 interface MonadZip<µ>: Monad<µ> {
-  fun <A, B> mzip(m: K1<µ, B>): K1<µ, Pair<A, B>>
-  fun <A, B, C> mzipWith(m: K1<µ, B>, f: ((A) -> B) -> C): K1<µ, C>
-  fun <A, B> munzip(f : K1<µ, Pair<A, B>>): Pair<K1<µ, A>, B>
+  fun <A, B> mzip(m1: K1<µ, A>, m2: K1<µ, B>): K1<µ, Pair<A, B>>
+  fun <A, B, C> mzipWith(m1: K1<µ, A>, m2: K1<µ, B>, f: (A, B) -> C): K1<µ, C>
+  fun <A, B> munzip(m : K1<µ, Pair<A, B>>): Pair<K1<µ, A>, K1<µ, B>>
 }
